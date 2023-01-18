d011eea41637a66cf9010339d78a461d044adf46
Xopin16 <rivas.fujos20@triana.salesianos.edu>
2023-01-18 06:19:32 +0100

    Agrego views

---
+++ parking-project/View/View.py
@@ -0,0 +1,14 @@
+def imprimir_menu_cliente():
+    print("1. Depositar vehiculo")
+    print("2. Retirar vehiculo")
+    print("3. Depositar abonados")
+    print("4. Retirar abonados")
+    print("0. Para salir")
+
+def imprimir_menu_admin():
+    print("1. Consultar estado parking")
+    print("2. Mostrar registro de facturas")
+    print("3. Consultar abonos")
+    print("4. Gestionar abonos")
+    print("5. Consultar caducidad de abonos")
+    print("0. Para salir")
