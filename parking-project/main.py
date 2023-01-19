7f3c454df1989ac4a1f63d4af5265651d868f39b
Xopin16 <rivas.fujos20@triana.salesianos.edu>
2023-01-19 06:42:45 +0100

    Arreglo while de main

--- parking-project/main.py
+++ parking-project/main.py
@@ -96,46 +96,50 @@ cobros_cliente_list = pickle.load(cobros_cliente)
 cobros_cliente.close()
 
 menu = 1
+menu1 = 1
+menu2 = 1
 while menu != 0:
     print("1. Cliente")
     print("2. Administrador")
     menu = int(input("Indica si eres cliente o administrador: "))
     if menu != 0:
         if menu == 1:
-            pk.mostrar_clientes(lista_clientes)
-            imprimir_menu_cliente()
-            menu1 = int(input("Indica desea hacer: "))
-            if menu1 == 1:
-                pk.mostrar_plazas_tipo(lista_clientes)
-                matricula = input("Introduzca matrícula: ")
-                tipo = input("Introduzca tipo de vehículo: ")
-                new_vehiculo = Vehiculo(matricula=matricula, tipo=tipo)
-                lista_vehiculos.append(new_vehiculo)
-                new_cliente = Cliente(vehiculo=new_vehiculo, plaza=None, pin=random.randint(100000, 999999))
-                lista_clientes.append(new_cliente)
-                ClienteService.depositar_vehiculo(pk, new_cliente, lista_plazas)
-            elif menu1 == 2:
-                ClienteService.retirar_vehiculo(lista_clientes, lista_cobros_cliente)
-            elif menu1 == 3:
-                ClienteService.depositar_abonados(lista_clientes)
-            elif menu1 == 4:
-                ClienteService.retirar_abonados(lista_clientes)
-            else:
-                print("Saliendo...")
+            while menu1 != 0:
+                pk.mostrar_clientes(lista_clientes)
+                imprimir_menu_cliente()
+                menu1 = int(input("Indica desea hacer: "))
+                if menu1 == 1:
+                    pk.mostrar_plazas_tipo(lista_clientes)
+                    matricula = input("Introduzca matrícula: ")
+                    tipo = input("Introduzca tipo de vehículo: ")
+                    new_vehiculo = Vehiculo(matricula=matricula, tipo=tipo)
+                    lista_vehiculos.append(new_vehiculo)
+                    new_cliente = Cliente(vehiculo=new_vehiculo, plaza=None, pin=random.randint(100000, 999999))
+                    lista_clientes.append(new_cliente)
+                    ClienteService.depositar_vehiculo(pk, new_cliente, lista_plazas)
+                elif menu1 == 2:
+                    ClienteService.retirar_vehiculo(lista_clientes, lista_cobros_cliente)
+                elif menu1 == 3:
+                    ClienteService.depositar_abonados(lista_clientes)
+                elif menu1 == 4:
+                    ClienteService.retirar_abonados(lista_clientes)
+                else:
+                    print("Saliendo...")
         elif menu == 2:
-            imprimir_menu_admin()
-            menu2 = int(input("Indica que desea hacer: "))
-            if menu2 == 1:
-                AdminService.controlar_estado_parking(pk, lista_plazas)
-            elif menu2 == 2:
-                AdminService.mostrar_facturacion(lista_cobros_cliente)
-            elif menu2 == 3:
-                AdminService.consular_abonados(lista_clientes)
-            elif menu2 == 4:
-                AdminService.gestion_abonos(lista_clientes, lista_plazas, lista_abonos)
-            elif menu2 == 5:
-                AdminService.caducidad_abonos(lista_clientes)
-            else:
-                print("Saliendo...")
+            while menu2 != 0:
+                imprimir_menu_admin()
+                menu2 = int(input("Indica que desea hacer: "))
+                if menu2 == 1:
+                    AdminService.controlar_estado_parking(pk, lista_plazas)
+                elif menu2 == 2:
+                    AdminService.mostrar_facturacion(lista_cobros_cliente)
+                elif menu2 == 3:
+                    AdminService.consular_abonados(lista_clientes)
+                elif menu2 == 4:
+                    AdminService.gestion_abonos(lista_clientes, lista_plazas, lista_abonos)
+                elif menu2 == 5:
+                    AdminService.caducidad_abonos(lista_clientes)
+                else:
+                    print("Saliendo...")
         else:
             print("Saliendo...")
